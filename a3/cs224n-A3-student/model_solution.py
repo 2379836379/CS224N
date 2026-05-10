6b4f3f06785f815d661c7c57cb2b484961c48dea
hachimi <231277751+2379836379@users.noreply.github.com>
2026-05-10 08:17:46 +0000

    reset author.

---
+++ a3/cs224n-A3-student/model_solution.py
@@ -0,0 +1,255 @@
+"""
+A bare-bones GPT-2 style transformer.
+"""
+
+import math
+from typing import Dict
+
+import torch
+from torch import nn, Tensor
+from torch.nn import functional as F
+from jaxtyping import Float, Int
+from torch.nn.functional import softmax
+from dataclasses import dataclass
+from einops import rearrange
+from transformers import GPT2LMHeadModel
+import huggingface_hub
+
+from utils import state_dict_converter
+
+
+# TODO: Add in attention mask to the entire assignment
+# TODO: Maybe add KV caching
+
+
+@dataclass
+class ModelConfig:
+    d_model: int
+    n_heads: int
+    n_layers: int
+    context_length: int
+    vocab_size: int
+
+
+class CausalAttention(nn.Module):
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+
+        # Using attention dim from attention is all you need
+        # d_model:隐藏维度,token向量长度; n_head:注意力头数 
+        assert config.d_model % config.n_heads == 0
+        # 每个attention维度
+        self.d_attention = int(config.d_model / config.n_heads)
+        self.n_heads = config.n_heads
+
+        #self.c_attn = nn.Linear(config.d_model, 3 * config.d_model)
+        # self.d_attention * config.n_heads = config.d_model
+        self.W_k = nn.Linear(config.d_model, self.d_attention * config.n_heads)
+        self.W_q = nn.Linear(config.d_model, self.d_attention * config.n_heads)
+        self.W_v = nn.Linear(config.d_model, self.d_attention * config.n_heads)
+
+        self.W_o = nn.Linear(self.d_attention * config.n_heads, config.d_model)
+
+        # Causal mask
+        # 维度: 1, 1, context_length, context_length
+        self.register_buffer(
+            "causal_mask",
+            torch.tril(torch.ones(config.context_length, config.context_length)).view(
+                1, 1, config.context_length, config.context_length
+            ),
+            persistent=False
+        )
+
+    def forward(
+        self, x: Float[Tensor, "batch seq_len d_model"]# 形状注释
+    ) -> Float[Tensor, "batch seq_len d_model"]:
+
+        # TODO, complete 
+        batch, seq_len, d_model = x.shape
+        # [B, T, d_model]
+        q = self.W_q(x)
+        k = self.W_k(x)
+        v = self.W_v(x)
+        #[B, T, H, D]
+        #[B, H, T, D]
+        q = q.view(batch, seq_len, self.n_heads, self.d_attention).transpose(1,2)
+        k = k.view(batch, seq_len, self.n_heads, self.d_attention).transpose(1,2)
+        v = v.view(batch, seq_len, self.n_heads, self.d_attention).transpose(1,2)
+        # [B, H, T, D]
+        attention_score = q @ k.transpose(-2, -1)
+        attention_score = attention_score/math.sqrt(self.d_attention)
+        mask = self.causal_mask[:, :, :seq_len, :seq_len]
+        attention_score = attention_score.masked_fill(mask == 0, float('-inf'))
+        # [B, H, T, T]
+        attention_out = softmax(attention_score, dim = -1)
+        # [B, H, T, D]
+        out = attention_out @ v
+        # [B, T, H, D]
+        out = out.transpose(1, 2)
+        out = out.contiguous().view(batch, seq_len, d_model)
+        out = self.W_o(out)
+        return out
+
+
+
+class GELU(nn.Module):
+    """
+    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT).
+    Reference: Gaussian Error Linear Units (GELU) paper: https://arxiv.org/abs/1606.08415
+    """
+
+    def forward(self, x: Float[Tensor, "..."]) -> Float[Tensor, "..."]:
+        return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))  # fmt: skip
+
+class MLP(nn.Module):
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+
+        self.fc1 = nn.Linear(config.d_model, 4 * config.d_model)
+        self.fc2 = nn.Linear(4 * config.d_model, config.d_model)
+        self.gelu = GELU()
+
+    def forward(
+        self, x: Float[Tensor, "batch seq_len d_model"]
+    ) -> Float[Tensor, "batch seq_len d_model"]:
+
+        # TODO, complete
+        out = self.fc2(self.gelu(self.fc1(x)))
+        return out
+        
+
+class DecoderBlock(nn.Module):
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+
+        self.mlp = MLP(config)
+        self.attention = CausalAttention(config)
+        self.pre_layer_norm = nn.LayerNorm(config.d_model)
+        self.post_layer_norm = nn.LayerNorm(config.d_model)
+
+    def forward(
+        self, x: Float[Tensor, "batch seq_len d_model"]
+    ) -> Float[Tensor, "batch seq_len d_model"]:
+
+        # TODO complete
+        out = x + self.attention(self.pre_layer_norm(x))
+        out = out + self.mlp(self.post_layer_norm(out))
+        return out
+
+
+class Transformer(nn.Module):
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+
+        self.config = config
+        self.embeddings = nn.Embedding(config.vocab_size, config.d_model)
+        self.position_embeddings = nn.Embedding(config.context_length, config.d_model)
+        self.backbone = nn.ModuleList([DecoderBlock(config) for _ in range(config.n_layers)])
+        self.final_layer_norm = nn.LayerNorm(config.d_model)
+        self.lm_head = nn.Linear(config.d_model, config.vocab_size, bias=False)
+
+        self._init_weights()
+
+    def _init_weights(self):
+
+        for module in self.modules():
+            if isinstance(module, nn.Linear):
+                torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+                if module.bias is not None:
+                    torch.nn.init.zeros_(module.bias)
+            elif isinstance(module, nn.Embedding):
+                torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            elif isinstance(module, nn.LayerNorm):
+                torch.nn.init.zeros_(module.bias)
+                torch.nn.init.ones_(module.weight)
+
+        # init all weights, and apply a special scaled init to the residual projections, per GPT-2 paper
+        for pn, p in self.named_parameters():
+            if pn.endswith("c_proj.weight"):
+                torch.nn.init.normal_(
+                    p, mean=0.0, std=0.02 / math.sqrt(2 * self.config.n_layers)
+                )
+
+    def forward(
+        self, x: Int[Tensor, "batch_size seq_len"]
+    ) -> Float[Tensor, "batch seq_len vocab_size"]:
+
+        # TODO, complete
+        batch_size, seq_len = x.shape
+        # 扩展维度
+        position = torch.arange(seq_len)
+        position = position.unsqueeze(0).expand(batch_size, seq_len)
+        x = self.embeddings(x) + self.position_embeddings(position)
+        
+
+        for block in self.backbone:
+            x = block(x)
+        x = self.final_layer_norm(x)
+        x = self.lm_head(x)
+        return x
+
+    @torch.no_grad()
+    def generate(
+        self,
+        x: Int[Tensor, "batch_size seq_len"],
+        num_new_tokens: int,
+    ) -> Int[Tensor, "batch_size seq_len+num_new_tokens"]:
+
+        # TODO, complete
+        for n in range(num_new_tokens):
+            logits = self(x)
+            next_token = logits[:, -1, :].argmax(dim = -1, keepdim = True)
+            x = torch.cat([x,next_token], dim = 1) 
+        return x
+
+
+    def get_loss_on_batch(
+        self,
+        input_ids: Int[Tensor, "batch_size seq_len"], 
+    ) -> Float[Tensor, ""]:
+
+        # TODO, complete
+        # B*T-1
+        logits = self(input_ids[:, :-1])
+        targets = input_ids[:, 1:]
+        # B*T-1*vocab 合并前两维
+        loss = F.cross_entropy(logits.reshape(-1, logits.size(-1)), targets.reshape(-1))
+        return loss
+
+
+    @classmethod
+    def from_pretrained(cls):
+        """
+        We simply always load up the GPT-2 model
+        """
+
+        # Config for GPT-2
+        config = ModelConfig(
+            d_model=768,
+            n_heads=12,
+            n_layers=12,
+            context_length=1024,
+            vocab_size=50257,
+        )
+
+        model = cls(config)
+
+        # Load weights from HuggingFace
+        model_hf = GPT2LMHeadModel.from_pretrained("gpt2")
+        converted_state_dict: Dict[str, Tensor] = state_dict_converter(model_hf.state_dict())
+
+        model.load_state_dict(converted_state_dict)
+
+        return model
+
+
+if __name__ == "__main__":
+
+    # Uncomment this if you are not logged in
+    # huggingface_hub.login()
+    
+    model = Transformer.from_pretrained()
